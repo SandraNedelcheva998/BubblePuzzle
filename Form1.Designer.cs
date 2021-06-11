b5f1ab6a7184391b9d02e1b11aed376cd1f2c31a
Сандра Неделчева <Sandra.Nedelcheva@students.finki.ukim.mk>
2021-06-11 20:48:19 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Puzzle
+{
+    partial class frmBubblePuzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbPuzzle = new System.Windows.Forms.GroupBox();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.gbImage = new System.Windows.Forms.GroupBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnAddImage = new System.Windows.Forms.Button();
+            this.tmrTimeElapsed = new System.Windows.Forms.Timer(this.components);
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.lblMovesDisplay = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gbPuzzle
+            // 
+            this.gbPuzzle.BackColor = System.Drawing.Color.Lavender;
+            this.gbPuzzle.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbPuzzle.Location = new System.Drawing.Point(33, 25);
+            this.gbPuzzle.Name = "gbPuzzle";
+            this.gbPuzzle.Size = new System.Drawing.Size(444, 444);
+            this.gbPuzzle.TabIndex = 1;
+            this.gbPuzzle.TabStop = false;
+            this.gbPuzzle.Text = "Puzzle Image";
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnEasy.Enabled = false;
+            this.btnEasy.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEasy.Location = new System.Drawing.Point(518, 111);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(136, 30);
+            this.btnEasy.TabIndex = 2;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = false;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnMedium.Enabled = false;
+            this.btnMedium.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedium.Location = new System.Drawing.Point(518, 157);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(136, 30);
+            this.btnMedium.TabIndex = 3;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseMnemonic = false;
+            this.btnMedium.UseVisualStyleBackColor = false;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
+            // btnHard
+            // 
+            this.btnHard.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnHard.Enabled = false;
+            this.btnHard.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHard.Location = new System.Drawing.Point(518, 206);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(136, 30);
+            this.btnHard.TabIndex = 4;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = false;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // gbImage
+            // 
+            this.gbImage.BackColor = System.Drawing.Color.Lavender;
+            this.gbImage.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbImage.Location = new System.Drawing.Point(695, 25);
+            this.gbImage.Name = "gbImage";
+            this.gbImage.Size = new System.Drawing.Size(300, 300);
+            this.gbImage.TabIndex = 5;
+            this.gbImage.TabStop = false;
+            this.gbImage.Text = "Final Image";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BackColor = System.Drawing.Color.Lavender;
+            this.lblTimer.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(711, 390);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(531, 156);
+            this.lblTimer.TabIndex = 7;
+            this.lblTimer.Text = "00:00:00";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Lavender;
+            this.btnReset.Enabled = false;
+            this.btnReset.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(518, 25);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(136, 38);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnAddImage
+            // 
+            this.btnAddImage.BackColor = System.Drawing.Color.Lavender;
+            this.btnAddImage.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddImage.Location = new System.Drawing.Point(518, 286);
+            this.btnAddImage.Name = "btnAddImage";
+            this.btnAddImage.Size = new System.Drawing.Size(136, 38);
+            this.btnAddImage.TabIndex = 0;
+            this.btnAddImage.Text = "New Image";
+            this.btnAddImage.UseVisualStyleBackColor = false;
+            this.btnAddImage.Click += new System.EventHandler(this.btnAddImage_Click);
+            // 
+            // tmrTimeElapsed
+            // 
+            this.tmrTimeElapsed.Enabled = true;
+            this.tmrTimeElapsed.Interval = 900;
+            this.tmrTimeElapsed.Tick += new System.EventHandler(this.tmrTimeElapsed_Tick);
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.BackColor = System.Drawing.Color.Lavender;
+            this.lblMoves.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoves.Location = new System.Drawing.Point(557, 389);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(134, 156);
+            this.lblMoves.TabIndex = 8;
+            this.lblMoves.Text = "0";
+            this.lblMoves.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMovesDisplay
+            // 
+            this.lblMovesDisplay.AutoSize = true;
+            this.lblMovesDisplay.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovesDisplay.Location = new System.Drawing.Point(555, 368);
+            this.lblMovesDisplay.Name = "lblMovesDisplay";
+            this.lblMovesDisplay.Size = new System.Drawing.Size(82, 33);
+            this.lblMovesDisplay.TabIndex = 9;
+            this.lblMovesDisplay.Text = "Moves";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(710, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 33);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Game Time";
+            // 
+            // frmBubblePuzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1030, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMovesDisplay);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.gbImage);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnEasy);
+            this.Controls.Add(this.gbPuzzle);
+            this.Controls.Add(this.btnAddImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmBubblePuzzle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bubble Puzzle";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.frmBubblePuzzle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddImage;
+        private System.Windows.Forms.GroupBox gbPuzzle;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.GroupBox gbImage;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer tmrTimeElapsed;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label lblMovesDisplay;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class frmBubblePuzzle : Form
+    {
+        PictureBox pbxFinalImage = null;
+        PictureBox pbxPuzzleImage = null;
+        PictureBox ClickBox1 = null;
+        PictureBox ClickBox2 = null;
+        PictureBox[] PuzzlePieces = null;
+
+        Image imageFinal;
+        Image imagePuzzle;
+        Image[] Images = null;
+
+        OpenFileDialog ofdFileDialog = null;
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+        int numberOfMoves = 0;
+        Point[] Locations = null;
+
+
+        public frmBubblePuzzle()
+        {
+            InitializeComponent();
+            lblTimer.Text = "00:00:00";
+            lblMoves.Text = "0";
+        }
+
+        private void btnAddImage_Click(object sender, EventArgs e)
+        {
+            btnEasy.Enabled = true;
+            btnMedium.Enabled = true;
+            btnHard.Enabled = true; 
+
+            if (ofdFileDialog == null)
+            {
+                ofdFileDialog = new OpenFileDialog();
+            }
+
+            if (ofdFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                if (pbxFinalImage == null)
+                {
+                    pbxFinalImage = new PictureBox();
+                    pbxFinalImage.Height = gbImage.Height;
+                    pbxFinalImage.Width = gbImage.Width;
+                    gbImage.Controls.Add(pbxFinalImage);
+                }
+                if (pbxPuzzleImage == null)
+                {
+                    pbxPuzzleImage = new PictureBox();
+                    pbxPuzzleImage.Height = gbPuzzle.Height;
+                    pbxPuzzleImage.Width = gbPuzzle.Width;
+                    gbPuzzle.Controls.Add(pbxPuzzleImage);
+                }
+                imageFinal = ProcessImage(Image.FromFile(ofdFileDialog.FileName), gbImage);
+                pbxFinalImage.Image = imageFinal;
+
+                imagePuzzle = ProcessImage(Image.FromFile(ofdFileDialog.FileName), gbPuzzle);
+                pbxPuzzleImage.Image = imagePuzzle;
+            }
+        }
+
+
+        private Bitmap ProcessImage(Image Image, GroupBox groupBox)
+        {
+            Bitmap bmpImg = new Bitmap(groupBox.Width, groupBox.Height);
+            Graphics graphics = Graphics.FromImage(bmpImg);
+            graphics.Clear(Color.White);
+            graphics.DrawImage(Image, new Rectangle(0, 0, groupBox.Width, groupBox.Height));
+            graphics.Flush();
+
+            return bmpImg;
+        }
+
+
+        private void SliceImage(Image image, Image[] images, int index, int numberRows, int numberColumns, int pieceX, int pieceY)
+        {
+            images[index] = new Bitmap(pieceX, pieceY);
+            Graphics graphics = Graphics.FromImage(images[index]);
+            graphics.Clear(Color.White);
+            graphics.DrawImage(image, new Rectangle(0,0,pieceX,pieceY), new Rectangle(pieceX * (index%numberColumns), pieceY * (index / numberColumns),pieceX,pieceY),GraphicsUnit.Pixel);
+            graphics.Flush();
+        }
+
+        private void Shuffle(ref int [] array)
+        {
+           Random rnd = new Random();
+           int i=array.Length;
+           while(i > 1)
+           {
+               int j = rnd.Next(i);  
+               i--;
+               int tmp = array[j];
+               array[j] = array[i];
+               array[i] = tmp;
+           }
+        }
+
+        public void PuzzleClick(object sender, EventArgs e)
+        {
+            if (ClickBox1 == null)
+            {
+                ClickBox1 = (PictureBox)sender;
+            }
+
+            else if (ClickBox2 == null)
+            {
+                ClickBox2 = (PictureBox)sender;
+                SwapImages(ClickBox1, ClickBox2);
+                ClickBox1 = null;
+                ClickBox2 = null;
+                numberOfMoves++;
+                lblMoves.Text = numberOfMoves.ToString();
+                if (IsComplete())
+                {
+                    Console.WriteLine("Puzzle Complete !!!");
+                    Win();
+                }
+            }
+            else
+            {
+                ClickBox1 = ClickBox2;
+                ClickBox2 = (PictureBox)sender;
+                SwapImages(ClickBox1, ClickBox2);
+                ClickBox1 = null;
+                ClickBox2 = null;
+                numberOfMoves++;
+                lblMoves.Text = numberOfMoves.ToString();
+            }
+        }
+
+
+        public void SwapImages(PictureBox picture1, PictureBox picture2)
+        {
+            Point temp = picture1.Location;
+            picture1.Location = picture2.Location;
+            picture2.Location = temp;
+        }
+
+
+        private bool IsComplete()
+        {
+            for (int i = 0; i < PuzzlePieces.Length; i++)
+            {
+                // Proveri dali sekoe delce e na tocnata pozicija spored mesanjeto na slozuvalkata
+                if (PuzzlePieces[i].Location != Locations[i])
+                    return false;
+            }
+            return true;
+        }
+
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            btnMedium.Enabled = false;
+            btnHard.Enabled = false;
+            btnReset.Enabled = true;
+            btnAddImage.Enabled = false;
+
+            tmrTimeElapsed.Start();
+            stopwatch.Start();
+
+            if (pbxPuzzleImage != null)
+            {
+                gbPuzzle.Controls.Remove(pbxPuzzleImage);
+                pbxPuzzleImage.Dispose();
+                pbxPuzzleImage = null;
+            }
+
+            if (pbxPuzzleImage == null)
+            {
+                Images = new Image[4];
+                PuzzlePieces = new PictureBox[4];
+                Locations = new Point[4];
+            } 
+
+            int pieceX = gbPuzzle.Width / 2;
+            int pieceY = gbPuzzle.Height / 2;
+            int[] tmpArray = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                tmpArray[i] = i;
+                if (PuzzlePieces[i] == null)
+                {
+                    PuzzlePieces[i] = new PictureBox();
+                    PuzzlePieces[i].Click +=new EventHandler(PuzzleClick);
+                    PuzzlePieces[i].BorderStyle = BorderStyle.Fixed3D;
+                }
+
+                PuzzlePieces[i].Height = pieceY;
+                PuzzlePieces[i].Width = pieceX;
+
+                SliceImage(imagePuzzle, Images, i, 2, 2, pieceX, pieceY);
+
+                PuzzlePieces[i].Location = new Point(pieceX * (i % 2), pieceY * (i / 2));
+                
+
+                if (!gbPuzzle.Controls.Contains(PuzzlePieces[i]))
+                {
+                    gbPuzzle.Controls.Add(PuzzlePieces[i]);
+                }
+            }
+
+            Shuffle(ref tmpArray);
+            for (int i = 0; i < 4; i++)
+            {
+                PuzzlePieces[i].Image = Images[tmpArray[i]];
+                Locations[i] = PuzzlePieces[tmpArray[i]].Location;
+            }
+               
+
+
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+            btnEasy.Enabled = false;
+            btnHard.Enabled = false;
+            btnReset.Enabled = true;
+            btnAddImage.Enabled = false;
+
+            tmrTimeElapsed.Start();
+            stopwatch.Start();
+
+            if (pbxPuzzleImage != null)
+            {
+                gbPuzzle.Controls.Remove(pbxPuzzleImage);
+                pbxPuzzleImage.Dispose();
+                pbxPuzzleImage = null;
+            }
+
+            if (pbxPuzzleImage == null)
+            {
+                Images = new Image[16];
+                PuzzlePieces = new PictureBox[16];
+                Locations = new Point[16];
+            }
+
+            int pieceX = gbPuzzle.Width / 4;
+            int pieceY = gbPuzzle.Height / 4;
+            int[] tmpArray = new int[16];
+            for (int i = 0; i < 16; i++)
+            {
+                tmpArray[i] = i;
+                if (PuzzlePieces[i] == null)
+                {
+                    PuzzlePieces[i] = new PictureBox();
+                    PuzzlePieces[i].Click += new EventHandler(PuzzleClick);
+                    PuzzlePieces[i].BorderStyle = BorderStyle.Fixed3D;
+                }
+
+                PuzzlePieces[i].Height = pieceY;
+                PuzzlePieces[i].Width = pieceX;
+
+                SliceImage(imagePuzzle, Images, i, 4, 4, pieceX, pieceY);
+
+                PuzzlePieces[i].Location = new Point(pieceX * (i % 4), pieceY * (i / 4));
+                if (!gbPuzzle.Controls.Contains(PuzzlePieces[i]))
+                {
+                    gbPuzzle.Controls.Add(PuzzlePieces[i]);
+                }
+            }
+
+            Shuffle(ref tmpArray);
+            for (int i = 0; i < 16; i++)
+            {
+                PuzzlePieces[i].Image = Images[tmpArray[i]];
+                Locations[i] = PuzzlePieces[tmpArray[i]].Location;
+            }
+
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+            btnMedium.Enabled = false;
+            btnEasy.Enabled = false;
+            btnReset.Enabled = true;
+            btnAddImage.Enabled = false;
+
+            tmrTimeElapsed.Start();
+            stopwatch.Start();
+
+            if (pbxPuzzleImage != null)
+            {
+                gbPuzzle.Controls.Remove(pbxPuzzleImage);
+                pbxPuzzleImage.Dispose();
+                pbxPuzzleImage = null;
+            }
+
+            if (pbxPuzzleImage == null)
+            {
+                Images = new Image[25];
+                PuzzlePieces = new PictureBox[25];
+                Locations = new Point[25];
+            }
+
+            int pieceX = gbPuzzle.Width / 5;
+            int pieceY = gbPuzzle.Height / 5;
+            int[] tmpArray = new int[25];
+            for (int i = 0; i < 25; i++)
+            {
+                tmpArray[i] = i;
+                if (PuzzlePieces[i] == null)
+                {
+                    PuzzlePieces[i] = new PictureBox();
+                    PuzzlePieces[i].Click += new EventHandler(PuzzleClick);
+                    PuzzlePieces[i].BorderStyle = BorderStyle.Fixed3D;
+                }
+
+                PuzzlePieces[i].Height = pieceY;
+                PuzzlePieces[i].Width = pieceX;
+
+                SliceImage(imagePuzzle, Images, i, 5, 5, pieceX, pieceY);
+
+                PuzzlePieces[i].Location = new Point(pieceX * (i % 5), pieceY * (i / 5));
+                if (!gbPuzzle.Controls.Contains(PuzzlePieces[i]))
+                {
+                    gbPuzzle.Controls.Add(PuzzlePieces[i]);
+                }
+            }
+
+            Shuffle(ref tmpArray);
+            for (int i = 0; i < 25; i++)
+            {
+                PuzzlePieces[i].Image = Images[tmpArray[i]];
+                Locations[i] = PuzzlePieces[tmpArray[i]].Location;
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btnMedium.Enabled = false;
+            btnHard.Enabled = false;
+            btnEasy.Enabled = false;
+            btnAddImage.Enabled = true;
+
+            DialogResult YesOrNo = new DialogResult();
+            if (lblTimer.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("Are You Sure ?", "Bubble Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+
+            if (YesOrNo == DialogResult.Yes || lblTimer.Text == "00:00:00")
+            {
+                // Izbrisi ja slikata Final
+                if (pbxFinalImage != null)
+                {
+                    gbImage.Controls.Remove(pbxFinalImage);
+                    pbxFinalImage.Dispose();
+                    pbxFinalImage = null;
+                }
+
+                // Izbrisi gi slikata Puzzle, ili malite Sliki delchinja na Puzzle, vo zavisnost sto ima vo leviot gbPuzzle
+                if (pbxPuzzleImage != null)
+                {
+                    gbPuzzle.Controls.Remove(pbxPuzzleImage);
+                    pbxPuzzleImage.Dispose();
+                    pbxPuzzleImage = null;
+                }
+                else
+                {
+
+                    for (int i = 0; i < PuzzlePieces.Length; i++)
+                    {
+                        gbPuzzle.Controls.Remove(PuzzlePieces[i]);
+                        PuzzlePieces[i] = null;
+                    }
+                    PuzzlePieces = null;
+                }
+                // Resetiraj go tajmerot i displeite za vreme i cekori
+                stopwatch.Reset();
+                lblTimer.Text = "00:00:00";
+                numberOfMoves = 0;
+                lblMoves.Text = "0";
+            }
+
+        }
+
+        private void Win ()
+        {
+            stopwatch.Stop();
+
+            DialogResult drOK = new DialogResult();
+            drOK = MessageBox.Show("Puzzle Solved !!", "Bubble Puzzle", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+           
+
+            if (drOK == DialogResult.OK)
+            {
+                // Izbrisi ja slikata Final
+                if (pbxFinalImage != null)
+                {
+                    gbImage.Controls.Remove(pbxFinalImage);
+                    pbxFinalImage.Dispose();
+                    pbxFinalImage = null;
+                }
+
+                // Izbrisi gi slikata Puzzle, ili malite Sliki delchinja na Puzzle, vo zavisnost sto ima vo leviot gbPuzzle
+                if (pbxPuzzleImage != null)
+                {
+                    gbPuzzle.Controls.Remove(pbxPuzzleImage);
+                    pbxPuzzleImage.Dispose();
+                    pbxPuzzleImage = null;
+                }
+                else
+                {
+
+                    for (int i = 0; i < PuzzlePieces.Length; i++)
+                    {
+                        gbPuzzle.Controls.Remove(PuzzlePieces[i]);
+                        PuzzlePieces[i] = null;
+                    }
+                    PuzzlePieces = null;
+                }
+
+                //Iskluci gi site kopcinja osven toa za doavanje nova slika
+                btnMedium.Enabled = false;
+                btnHard.Enabled = false;
+                btnEasy.Enabled = false;
+                btnReset.Enabled = false;
+                btnAddImage.Enabled = true;
+
+                // Resetiraj go tajmerot i displeite za vreme i cekori
+                stopwatch.Reset();
+                lblTimer.Text = "00:00:00";
+                numberOfMoves = 0;
+                lblMoves.Text = "0";
+            }
+
+        }
+
+        private void frmBubblePuzzle_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrTimeElapsed_Tick(object sender, EventArgs e)
+        {
+            if (stopwatch.Elapsed.ToString() != "00:00:00")
+                lblTimer.Text = stopwatch.Elapsed.ToString().Remove(8);
+        }
+
+        
+    }
+}
